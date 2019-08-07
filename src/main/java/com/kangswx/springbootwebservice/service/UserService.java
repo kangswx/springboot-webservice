91e8dea6d334acb0c6f6d89322e7965df8c4b8c2
kangswx <996334837@qq.com>
2019-08-07 11:48:01 +0800

    新增相关的注释

--- src/main/java/com/kangswx/springbootwebservice/service/UserService.java
+++ src/main/java/com/kangswx/springbootwebservice/service/UserService.java
@@ -5,10 +5,14 @@ import javax.jws.WebParam;
 import javax.jws.WebResult;
 import javax.jws.WebService;
 
+/**
+ * WebService接口
+ * WebService(targetNamespace = "http://service.springbootwebservice.kangswx.com")如果不添加的话,动态调用invoke的时候,会报找不到接口内的方法
+ */
 @WebService(targetNamespace = "http://service.springbootwebservice.kangswx.com")
 public interface UserService {
 
-    @WebMethod
+    @WebMethod  //标注该方法为webservice暴露的方法,用于向外公布，它修饰的方法是webservice方法
     String getUser(@WebParam(name = "id") int id);
 
     @WebMethod
